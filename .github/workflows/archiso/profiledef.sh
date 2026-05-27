e46fb5e68da8ea45c6acaa7df0bf08eabb46994f
zamkara <zamkara@gnuweeb.org>
2026-05-27 18:47:58 +0700

    Fix: locale generation script syntax and remove annoying desktop icon from user skel

--- .github/workflows/archiso/profiledef.sh
+++ .github/workflows/archiso/profiledef.sh
@@ -30,7 +30,6 @@ file_permissions=(
   ["/etc/sudoers"]="0:0:400"
   ["/etc/locale.gen"]="0:0:644"
   ["/usr/bin/alga"]="0:0:755"
-  ["/etc/skel/Desktop/com.zamkara.alga.desktop"]="0:0:755"
   ["/usr/share/applications/com.zamkara.alga.desktop"]="0:0:755"
   ["/etc/pacman.d/hooks/99-hide-apps.hook"]="0:0:644"
 )
