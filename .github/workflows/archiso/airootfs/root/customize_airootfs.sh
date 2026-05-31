1e14c870451658b65d2f0e9e6900bbdd9319d86f
zamkara <zamkara@gnuweeb.org>
2026-05-31 08:10:38 +0700

    fix: hide more unwanted apps in ISO

--- .github/workflows/archiso/airootfs/root/customize_airootfs.sh
+++ .github/workflows/archiso/airootfs/root/customize_airootfs.sh
@@ -6,7 +6,7 @@ sed -i 's/airootfs\.sfs/originium.sfs/g' /usr/lib/initcpio/hooks/archiso || true
 sed -i 's/airootfs\.sha512/originium.sha512/g' /usr/lib/initcpio/hooks/archiso || true
 
 # Hide unwanted apps
-rm -f /usr/share/applications/{bssh,bvnc,avahi-discover,qv4l2,qvidcap,stoken-gui,stoken-gui-small,org.gnome.Extensions,lstopo}.desktop || true
+rm -f /usr/share/applications/{bssh,bvnc,avahi-discover,qv4l2,qvidcap,stoken-gui,stoken-gui-small,org.gnome.Extensions,org.gnome.TextEditor,nvidia-settings,lstopo}.desktop || true
 
 # Compile schemas to ensure MoreWaita and app folders apply
 glib-compile-schemas /usr/share/glib-2.0/schemas || true
